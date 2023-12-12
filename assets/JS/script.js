b0b215e64c1da7de657d255d9800dc1f6aff1989
LukeWalls22 <dimuroluca@hotmail.it>
2023-12-12 17:30:40 +0100

    struttura base

---
+++ assets/JS/script.js
@@ -0,0 +1,12 @@
+
+// FETCH Base con autorizzazione
+
+fetch("https://striveschool-api.herokuapp.com/api/put-your-endpoint-here/", {
+headers: {
+"Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTc4ODg2ZjI2NzYxNDAwMTgzYzMzMGEiLCJpYXQiOjE3MDIzOTgwNjMsImV4cCI6MTcwMzYwNzY2M30.CcNypLUrqv8aro_wBllsU1HlkTZGxmbHOSL_6bVdMyg"
+}
+})
+
+const authKey = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTc4ODg2ZjI2NzYxNDAwMTgzYzMzMGEiLCJpYXQiOjE3MDIzOTgwNjMsImV4cCI6MTcwMzYwNzY2M30.CcNypLUrqv8aro_wBllsU1HlkTZGxmbHOSL_6bVdMyg";
+
+
